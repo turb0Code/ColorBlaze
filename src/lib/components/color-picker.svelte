7ab7e2dbe138c9fc003738281fe13895fb5139d9
turb0Code <turb000.max.10@gmail.com>
2024-01-09 22:11:56 +0100

    Changed some files, prepared for UI

---
+++ src/lib/components/color-picker.svelte
@@ -0,0 +1,29 @@
+<script lang="js">
+  import { writable } from "svelte/store";
+  import { onMount } from "svelte";
+  import { Button } from "flowbite-svelte";
+
+  let picker1;
+  let picker;
+  onMount(() => {
+    picker1.opened = true;
+    picker = writable(picker1);
+    console.log(picker1.state.isPopupVisible);
+    picker.subscribe(() => {
+      console.log(picker1);
+    });
+  });
+
+  import "$lib/scripts/toolcool-color-picker.min.js";
+</script>
+
+<toolcool-color-picker
+  bind:this={picker1}
+  id="color-picker-1"
+  color="rgb(90, 60, 90)"
+  button-width="0rem"
+  button-height="0rem"
+  class="z-1"
+></toolcool-color-picker>
+
+<Button color="alternative" class="mt-60">R</Button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,26 @@
 <script lang="js">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import { writable } from 'svelte/store';
-	import { onMount } from 'svelte';
-
-	let picker1;
-	let picker;
-	onMount(() => {
-		picker1.opened = true;
-		picker = writable(picker1)
-		console.log(picker1.state.isPopupVisible)
-		picker.subscribe(() => { console.log(picker1) })
-	})
-
-	import '$lib/scripts/toolcool-color-picker.min.js';
+  import ColorPicker from "../lib/components/color-picker.svelte";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
+<div class="grid grid-cols-2 grid-rows-1">
+  <div>
+    <ColorPicker></ColorPicker>
+  </div>
 
-	<toolcool-color-picker bind:this={picker1} id="color-picker-1" color="rgb(90, 60, 90)" button-width="0rem" button-height="0rem"></toolcool-color-picker>
-
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+  <div></div>
+</div>
 
 <style>
-
-	:root {
-		--tool-cool-color-picker-btn-bg: rgba(0, 0, 0, 0);
-		--tool-cool-color-picker-btn-border-color: rgba(0, 0, 0, 0);
-		--tool-cool-color-picker-btn-border-color-inner: rgba(0, 0, 0, 0);
-		--tool-cool-color-picker-btn-border-radius: 0rem;
-		--tool-cool-color-picker-btn-border-radius-inner: 0rem;
-	}
-
-	toool-cool-color-picker {
-		display: block !important;
-	}
-
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  :root {
+    --tool-cool-color-picker-btn-bg: rgba(0, 0, 0, 0);
+    --tool-cool-color-picker-btn-border-color: rgba(0, 0, 0, 0);
+    --tool-cool-color-picker-btn-border-color-inner: rgba(0, 0, 0, 0);
+    --tool-cool-color-picker-btn-border-radius: 0rem;
+    --tool-cool-color-picker-btn-border-radius-inner: 0rem;
+  }
 </style>
