7819ae81df8421565feb036d34866678fd95a9a5
turb0Code <turb000.max.10@gmail.com>
2024-01-11 20:35:08 +0100

    Added some color picker

--- src/lib/components/color-space.svelte
+++ src/lib/components/color-space.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Button } from "flowbite-svelte";
-  import { HslToRgb } from "$lib/scripts/color-conversion.js";
+  //import { HslToRgb } from "$lib/scripts/color-conversion.js";
 </script>
 
 <div class="w-[14.5rem] flex flex-row mt-60 justify-evenly">
