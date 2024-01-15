2bb6eda61deb9897a6ccf9c8d3c7d981ac10d474
KRATA46 <jakubgalarda@gmail.com>
2024-01-15 22:07:07 +0100

    added basic navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,6 +3,22 @@
   import logo from "$lib/images/svelte-logo.svg";
   import github from "$lib/images/github.svg";
   import { DarkMode } from "flowbite-svelte";
+
+  import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu } from 'flowbite-svelte';
+  import { ChevronDownOutline } from 'flowbite-svelte-icons';
+  let menu = [
+    { name: 'About us', href: '/about' },
+    { name: 'Blog', href: '/blog' },
+    { name: 'Contact us', href: '/contact' },
+    { name: 'Library', href: '/library' },
+    { name: 'Newsletter', href: '/news' },
+    { name: 'Support Center', href: '/support' },
+    { name: 'Resources', href: '/resource' },
+    { name: 'Playground', href: '/play' },
+    { name: 'Terms', href: '/terms' },
+    { name: 'Pro Version', href: '/pro' },
+    { name: 'License', href: '/license' }
+  ];
 </script>
 
 <header>
@@ -14,6 +30,25 @@
   <DarkMode size="md"></DarkMode>
 </header>
 
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi class="cursor-pointer">
+      Mega menu<ChevronDownOutline class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline" />
+    </NavLi>
+    <MegaMenu items={menu} let:item>
+      <a href={item.href} class="hover:text-primary-600 dark:hover:text-primary-500">{item.name}</a>
+    </MegaMenu>
+    <NavLi href="/services">Services</NavLi>
+    <NavLi href="/services">Products</NavLi>
+    <NavLi href="/services">Contact</NavLi>
+  </NavUl>
+</Navbar>
 <style>
   header {
     display: flex;
