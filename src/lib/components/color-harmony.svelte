eebe7ea02a9a4fd2c1a0bf64526bd314a32a370a
turb0Code <turb000.max.10@gmail.com>
2024-01-15 22:57:07 +0100

    Changed NavBar, ColorHarmony and styles.

--- src/lib/components/color-harmony.svelte
+++ src/lib/components/color-harmony.svelte
@@ -1,31 +1,28 @@
 <script>
-    import { writable, derived } from "svelte/store";
-    import { Select, Label } from "flowbite-svelte";
-    let selected;
-    let countries = [
-      { value: "yuv", name: "YUV" },
-      { value: "yiq", name: "YIQ" },
-      { value: "xyz", name: "XYZ" },
-      { value: "xyz", name: "LAB" },
-      { value: "xyz", name: "LUV" },
-      { value: "xyz", name: "XYZ" },
-    ];
-  
-  </script>
-  
-  <Label for="countries" class="w-[14.5rem] mt-4">Select an option</Label>
-  <Select
-    id="countries"
-    class="mt-2 w-[14.5rem]"
-    bind:value={selected}
-    placeholder=""
-  >
-    {#each countries as { value, name }}
-      <option {value} class="h-8">
-        {name}
-        <div class="h-4 flex"></div>
-        <div></div>
-        <div></div>
-      </option>
-    {/each}
-  </Select>
+  import { Select, Label } from "flowbite-svelte";
+  let selected;
+  let countries = [
+    { value: "mono", name: "Monochromatic" },
+    { value: "cont", name: "Contrast" },
+    { value: "soft", name: "Soft Contrast" },
+    { value: "dbct", name: "Double Contrast" },
+    { value: "anlg", name: "Analogous" },
+  ];
+</script>
+
+<Label for="countries" class="w-[14.5rem] mt-4">Select an option</Label>
+<Select
+  id="countries"
+  class="mt-2 w-[14.5rem]"
+  bind:value={selected}
+  placeholder=""
+>
+  {#each countries as { value, name }}
+    <option {value} class="h-8">
+      {name}
+      <div class="h-4 flex"></div>
+      <div></div>
+      <div></div>
+    </option>
+  {/each}
+</Select>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,135 +4,58 @@
   import github from "$lib/images/github.svg";
   import { DarkMode } from "flowbite-svelte";
 
-  import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu } from 'flowbite-svelte';
-  import { ChevronDownOutline } from 'flowbite-svelte-icons';
+  import {
+    Navbar,
+    NavBrand,
+    NavHamburger,
+    NavUl,
+    NavLi,
+    MegaMenu,
+  } from "flowbite-svelte";
+  import { ChevronDownOutline } from "flowbite-svelte-icons";
   let menu = [
-    { name: 'About us', href: '/about' },
-    { name: 'Blog', href: '/blog' },
-    { name: 'Contact us', href: '/contact' },
-    { name: 'Library', href: '/library' },
-    { name: 'Newsletter', href: '/news' },
-    { name: 'Support Center', href: '/support' },
-    { name: 'Resources', href: '/resource' },
-    { name: 'Playground', href: '/play' },
-    { name: 'Terms', href: '/terms' },
-    { name: 'Pro Version', href: '/pro' },
-    { name: 'License', href: '/license' }
+    { name: "About us", href: "/about" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact us", href: "/contact" },
+    { name: "Library", href: "/library" },
+    { name: "Newsletter", href: "/news" },
+    { name: "Support Center", href: "/support" },
+    { name: "Resources", href: "/resource" },
+    { name: "Playground", href: "/play" },
+    { name: "Terms", href: "/terms" },
+    { name: "Pro Version", href: "/pro" },
+    { name: "License", href: "/license" },
   ];
 </script>
 
-<header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
-      <img src={logo} alt="SvelteKit" />
-    </a>
-  </div>
-  <DarkMode size="md"></DarkMode>
-</header>
-
 <Navbar let:hidden let:toggle>
   <NavBrand href="/">
-    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    <img
+      src="/images/flowbite-svelte-icon-logo.svg"
+      class="me-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+      >Flowbite</span
+    >
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
     <NavLi href="/">Home</NavLi>
     <NavLi class="cursor-pointer">
-      More Examples<ChevronDownOutline class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline" />
+      More Examples<ChevronDownOutline
+        class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline"
+      />
     </NavLi>
     <MegaMenu items={menu} let:item>
-      <a href={item.href} class="hover:text-primary-600 dark:hover:text-primary-500">{item.name}</a>
+      <a
+        href={item.href}
+        class="hover:text-primary-600 dark:hover:text-primary-500"
+        >{item.name}</a
+      >
     </MegaMenu>
     <NavLi href="/services">Contact</NavLi>
+    <DarkMode size="md"></DarkMode>
   </NavUl>
 </Navbar>
-<style>
-  header {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .corner {
-    width: 3em;
-    height: 3em;
-  }
-
-  .corner a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
-
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
-  }
-
-  nav {
-    display: flex;
-    justify-content: center;
-    --background: rgba(255, 255, 255, 0.7);
-  }
-
-  svg {
-    width: 2em;
-    height: 3em;
-    display: block;
-  }
-
-  path {
-    fill: var(--background);
-  }
-
-  ul {
-    position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    list-style: none;
-    background: var(--background);
-    background-size: contain;
-  }
-
-  li {
-    position: relative;
-    height: 100%;
-  }
-
-  li[aria-current="page"]::before {
-    --size: 6px;
-    content: "";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
-  }
-
-  nav a {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    padding: 0 0.5rem;
-    color: var(--color-text);
-    font-weight: 700;
-    font-size: 0.8rem;
-    text-transform: uppercase;
-    letter-spacing: 0.1em;
-    text-decoration: none;
-    transition: color 0.2s linear;
-  }
-
-  a:hover {
-    color: var(--color-theme-1);
-  }
-</style>
