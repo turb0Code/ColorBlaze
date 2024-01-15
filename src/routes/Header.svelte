58d6047c9b19d5fdb6fe681a7efa7a996d030e3c
KRATA46 <jakubgalarda@gmail.com>
2024-01-15 22:13:53 +0100

    added something

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -39,7 +39,7 @@
   <NavUl {hidden}>
     <NavLi href="/">Home</NavLi>
     <NavLi class="cursor-pointer">
-      Mega menu<ChevronDownOutline class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline" />
+      More Examples<ChevronDownOutline class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline" />
     </NavLi>
     <MegaMenu items={menu} let:item>
       <a href={item.href} class="hover:text-primary-600 dark:hover:text-primary-500">{item.name}</a>
