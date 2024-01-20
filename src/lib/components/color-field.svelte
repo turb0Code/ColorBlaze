98a3dc0d34232bc18c25e4fb841b34a8a0df1a13
turb0Code <turb000.max.10@gmail.com>
2024-01-20 23:45:18 +0100

    Made text on ColorField reactive.

--- src/lib/components/color-field.svelte
+++ src/lib/components/color-field.svelte
@@ -1,7 +1,13 @@
 <script>
+import { l } from "$lib/scripts/stores.js";
+
 export let hex = "#0e483c";
+
+let textColor = "#efefef";
+
+l.subscribe(() => { textColor = $l > 50 ? "#0e0e0f" : "#efefef"; });
 </script>
 
 <div class="border-2 border-dark dark:border-light text-center h-40 w-40 rounded-md flex align-middle items-center justify-center mt-4" style="background-color: {hex};">
-    <button class="p-0" >{hex}</button>
+    <button class="p-0" style="color: {textColor}">{hex}</button>
 </div>
