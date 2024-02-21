4cdf08033140203275ce01efb4210eea8fdcbe8b
KRATA46 <jakubgalarda@gmail.com>
2024-02-21 07:36:24 +0100

    Imported buisness card

--- src/routes/images/+page.svelte
+++ src/routes/images/+page.svelte
@@ -3,4 +3,5 @@
     <img src="src/lib/images/logo.svg" alt="logo" class="w-96">
     <img src="src/lib/images/milk.svg" alt="milk" class="w-96">
     <img src="src/lib/images/Janek.svg" alt="Janek" class="w-96">
+    <img src="src/lib/images/taxi.svg" alt="taxi" class="">
 </div>
